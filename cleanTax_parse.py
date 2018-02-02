7dc3a53f2f4e020632ee6d896b40a744c9518093
Sahil Gupta <sahil1105@hotmail.com>
2018-02-02 11:19:58 -0600

    got taxonomy pasrsing working using anytree

--- cleanTax_parse.py
+++ cleanTax_parse.py
@@ -13,17 +13,20 @@ import os
 import string
 import argparse
 import pickle
+from anytree import Node, RenderTree
 
 
 class AntlrCleanTaxListener(CleanTaxListener):
 
 	def __init__(self):
-		self.data = {'taxonomies' : [], 
+		self.data = {'taxonomies' : {}, 
 					 'articulations': [], 
 					 'current_taxonomy': None,
 					 'current_articulation': None}
 
 	def exitCt_input(self, ctx):
+		#print (self.data['taxonomies'])
+		#print (self.data['current_taxonomy'])
 		self.data = {}
 
 	def enterTax_desc(self, ctx):
@@ -31,10 +34,59 @@ class AntlrCleanTaxListener(CleanTaxListener):
 		tax_name = []
 		i = 0
 		while ctx.TEXT(i) is not None:
-			tax_name.append(ctx.TEXT(i))
+			tax_name.append(ctx.TEXT(i).getText())
 			i += 1
 
 		self.data['current_taxonomy'] = tax_name[0]
+		print (tax_name)
+
+		self.data['taxonomies'][tax_name[0]] = {tax_name[0]: Node(tax_name[0])}
+
+
+	def enterTax_sub_desc(self, ctx):
+
+		# print ("HERE")
+		# print (self.data['current_taxonomy'])
+		# print (self.data['taxonomies'])
+
+		i = 0
+		parent = None
+		children = []
+		while ctx.TEXT(i) is not None:
+
+			node_name = ctx.TEXT(i).getText().strip()
+			#print (node_name)
+			if i == 0:
+				parent = node_name
+			else:
+				children.append(node_name)
+
+			i+=1
+
+
+		#print ("parent:", parent)
+		#print ("children:", children)
+
+		if (parent != None) and (parent not in self.data['taxonomies'][self.data['current_taxonomy']]):
+
+			self.data['taxonomies'][self.data['current_taxonomy']][parent] = Node(parent, parent = self.data['taxonomies'][self.data['current_taxonomy']][self.data['current_taxonomy']])
+
+		for child in children:
+
+			self.data['taxonomies'][self.data['current_taxonomy']][child] = Node(child, parent = self.data['taxonomies'][self.data['current_taxonomy']][parent])
+
+
+	def exitTax_desc(self, ctx):
+
+		print(RenderTree(self.data['taxonomies'][self.data['current_taxonomy']][self.data['current_taxonomy']]))
+
+		self.data['current_taxonomy'] = None
+
+
+
+
+
+
 
 
         
@@ -50,4 +102,7 @@ lexer = CleanTaxLexer(input)
 stream = CommonTokenStream(lexer)
 parser = CleanTaxParser(stream)
 tree = parser.ct_input()
-print (Trees.toStringTree(tree, None, parser))
+#print (Trees.toStringTree(tree, None, parser))
+lean_euler = AntlrCleanTaxListener()
+walker = ParseTreeWalker()
+walker.walk(lean_euler, tree)
