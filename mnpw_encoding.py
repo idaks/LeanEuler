e4c031aebf998a92f21e83721a7680edf155ee24
sjgupta2 <sjgupta2@illinois.edu>
2018-04-15 12:36:06 -0500

    added a few more mnpw code

--- mnpw_encoding.py
+++ mnpw_encoding.py
@@ -18,10 +18,50 @@ def gen_node_name(node_name: str):
 
 	return "\"{}\"".format(node_name.replace(".", "_"))
 
+def count_non_zero(t1: str, t2: str, n1: str, n2: str):
+
+	return ":- #count {{X : vrs(X), {}({}, X), {}({}, X)}} <= 0.".format(t1, nn1, t2, nn2)
+
+def count_non_zero_in_out(nn1: str, nn2: str):
+
+	return count_non_zero("in", "out", nn1, nn2)
+
+def count_nonzero_in_in(nn1: str, nn2: str):
+
+	return count_non_zero("in", "in", nn1, nn2)
+
+def count_nonzero_out_out(nn1: str, nn2: str):
+
+	return count_non_zero("out", "out", nn1, nn2)
+
+def count_nonzero_out_in(nn1: str, nn2: str):
+
+	return count_non_zero_in_out(nn2, nn1)
+
+def ir_helper(t1: str, t2: str, n1: str, n2: str, idx: int):
+
+	return "ir(X, r{}) :- {}({}, X), {}({}, X).".format(idx, t1, n1, t2, n2)
+
+def ir_in_out(n1: str, n2: str, idx: int):
+
+	return ir_helper("in", "out", n1, n2, idx)
+
+def ir_in_in(n1: str, n2: str, idx: int):
+
+	return ir_helper("in", "in", n1, n2, idx)
+
+def ir_out_out(n1: str, n2: str, idx: int):
+
+	return ir_helper("out", "out", n1, n2, idx)
+
+def ir_out_in(n1: str, n2: str, idx: int):
+
+	return ir_in_out(n2, n1, idx)
+
 def gen_isa_rule(n1: str, n2: str, idx: int):
 
-	r1 = "ir(X, r{}) :- in({}, X), out({}, X).".format(idx, n1, n2)
-	r2 = ":- #count {{X : vrs(X), in({}, X), in({}, X)}} <= 0.".format(n1, n2)
+	r1 = ir_in_out(n1, n2, idx) #"ir(X, r{}) :- in({}, X), out({}, X).".format(idx, n1, n2)
+	r2 = count_non_zero_in_in(n1, n2) #":- #count {{X : vrs(X), in({}, X), in({}, X)}} <= 0.".format(n1, n2)
 	return "\n".join([r1,r2])
 
 def gen_coverage_rule(parent: str, children: list):
@@ -33,12 +73,9 @@ def gen_coverage_rule(parent: str, children: list):
 
 def gen_sibling_disjointness(n1: str, n2: str, idx: int):
 
-	def in_out_helper(nn1, nn2):
-		return ":- #count {{X : vrs(X), in({}, X), out({}, X)}} <= 0.".format(nn1, nn2)
-
-	r1 = "ir(X, r{}) :- in({}, X), in({}, X).".format(idx, n1, n2)
-	r2 = in_out_helper(n1, n2)
-	r3 = in_out_helper(n2, n1)
+	r1 = ir_in_in(n1, n2, idx) #"ir(X, r{}) :- in({}, X), in({}, X).".format(idx, n1, n2)
+	r2 = count_nonzero_in_out(n1, n2)
+	r3 = count_nonzero_in_out(n2, n1)
 
 	return "\n".join([r1,r2,r3])
 
@@ -66,6 +103,45 @@ def gen_region_meanings():
 
 	return "\n".join([in_rule, out_rule, exactly_one, irs_rule])
 
+def gen_congruence_rel(n1: str, n2: str, idx: int):
+
+	r1 = ir_out_in(n1, n2, idx) #"ir(X, r{}) :- out({}, X), in({}, X).".format(idx, n1, n2)
+	r2 = ir_out_in(n2, n1, idx) #"ir(X, r{}) :- out({}, X), in({}, X).".format(idx, n2, n1)
+	r3 = count_non_zero_in_in(n1, n2) #":- #count{{X : vrs(X), in({}, X), in({}, X)}} <= 0.".format(n1, n2)
+
+	return "\n".join([r1, r2, r3])
+
+def gen_proper_part_rel(n1: str, n2: str, idx: int):
+
+	r1 = ir_in_out(n1, n2, idx) #"ir(X, r{}) :- in({}, X), out({}, X).".format(idx, n1, n2)
+	r2 = count_non_zero_in_in(n1, n2) #":- #count{{X : vrs(X), in({}, X), in({}, X)}} = 0.".format(n1, n2)
+	r3 = count_non_zero_out_in(n1, n2) #":- #count{{X : vrs(X), out({}, X), in({}, X)}} = 0.".format(n1, n2)
+
+	return "\n".join([r1, r2, r3])
+
+def gen_proper_part_reverse_rel(n1: str, n2: str, idx: int):
+
+	r1 = ir_out_in(n1, n2, idx) #"ir(X, r{}) :- out({}, X), in({}, X).".format(idx, n1, n2)
+	r2 = count_non_zero_in_out(n1, n2) #":- #count{{X : vrs(X), in({}, X), out({}, X)}} = 0.".format(n1, n2)
+	r3 = count_non_zero_in_in(n1, n2) #":- #count{{X : vrs(X), in({}, X), in({}, X)}} = 0.".format(n1, n2)
+
+	return "\n".join([r1, r2, r3])
+
+def gen_partial_overlap_rel(n1: str, n2: str):
+
+	r1 = count_non_zero_in_out(n1, n2) #":- #count{{X : vrs(X), in({}, X), out({}, X)}} = 0.".format(n1, n2)
+	r2 = count_non_zero_in_in(n1, n2) #":- #count{{X : vrs(X), in({}, X), in({}, X)}} = 0.".format(n1, n2)
+	r3 = count_non_zero_out_in(n1, n2) #":- #count{{X : vrs(X), out({}, X), in({}, X)}} = 0.".format(n1, n2)
+
+	return "\n".join([r1, r2, r3])
+
+def gen_disjoint_rel(n1: str, n2: str, idx: int)
+
+	r1 = count_non_zero_in_out(n1, n2)
+	r2 = ir_in_in(n1, n2, idx)
+	r3 = count_non_zero_out_in(n1, n2)
+
+	return "\n".join([r1, r2, r3])
 
 
 
