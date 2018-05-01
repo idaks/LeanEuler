b9635d5a1619119fadf9bafc9bcbb5327226f5fb
sjgupta2 <sjgupta2@illinois.edu>
2018-05-01 08:41:56 -0500

    need to fix euler bit logic and some other bugs

--- mnpw_encoding.py
+++ mnpw_encoding.py
@@ -69,9 +69,10 @@ def vr_ir_helper(rel1: str, rel2: str, n1: str, n2: str, idx: int, rel_var="X"):
     return "vr({}, r{}) ; ".format(rel_var, idx) + ir_helper(rel1, rel2, n1, n2, idx, rel_var)
 
 def gen_coverage_rule(parent: str, children: list):  # For every non-leaf node
+    header = '% Coverage for {}'.format(parent)
     lhs = 'out({}, X)'.format(parent)
     rhs = ", ".join(list(map(lambda x: 'out({}, X)'.format(x), children)))
-    return "{} :- {}.".format(lhs, rhs)
+    return "{}\n{} :- {}.\n".format(header, lhs, rhs)
 
 def gen_concept2_rule(node: str, tax_num: int):
     return "concept2({}, {}).".format(node, tax_num)
@@ -83,38 +84,43 @@ def concept2_concept_rel():
     return "concept2(A, B) :- concept(A,B,_)."
 
 def gen_sibling_disjointness(n1: str, n2: str, idx: int):  # For every pair of siblings
+    header = '% {} ! {}'.format(n1, n2)
     r1 = ir_helper(n1=n1, n2=n2, rel1="in", rel2="in", idx=idx)
     r2 = ":- {}.".format(not_filter_helper(n1=n1, n2=n2, rel1="in", rel2="out", sign="=", r_var="X"))
     r3 = ":- {}.".format(not_filter_helper(n1=n2, n2=n1, rel1="in", rel2="out", sign="=", r_var="X"))
-    return "\n".join([r1,r2,r3])
+    return "\n".join([header, r1,r2,r3, ''])
 
 def gen_isa_rule(child: str, parent: str, idx: int):  # For every parent-child relation
+    header = '% {} isa {}'.format(child, parent)
     r1 = ir_helper(n1=child, n2=parent, rel1="in", rel2="out", idx=idx)
     r2 = ":- {}.".format(not_filter_helper(n1=child, n2=parent, rel1="in", rel2="in", sign="=", r_var="X"))
-    return "\n".join([r1,r2])
+    return "\n".join([header, r1,r2, ''])
 
 def gen_euler_bit():  # Standard
+    header = '%  Euler Bit'
     r1 = "bit(M, {}, V) :- r(M), M1=M/1, V = M1 \ 5.".format(0)
     r2 = "bit(M, {}, V) :- r(M), M1=M/5, V = M1 \ 5.".format(1)
-    return [r1, r2]
+    return [header, r1, r2, '']
 
 def gen_region_meanings():  # Standard
+    header = "% Region Meanings"
     in_rule =   "in(X, M) :- r(M), concept(X, T, N), N1=N+1, bit(M, T, N1)."
     out_rule = "out(X, M) :- r(M), concept(X, T, N), N1=N+1, not bit(M, T, N1)."
     exactly_one = "in(X, M) :- r(M), concept2(X, _), not out(X, M)."
     irs_rule = "irs(M) :- in(X, M), out(X, M), r(M), concept2(X, _)."
-    return [in_rule, out_rule, exactly_one, irs_rule]
+    return [header, in_rule, out_rule, exactly_one, irs_rule, '']
 
 def gen_region_constraints():  # Standard
+    header = "% Region Constraints"
     irs = "irs(X) :- ir(X, _)."
     vrs = "vrs(X) :- vr(X, _)."
     vr = "vr(X, X) :- not irs(X), r(X)."
     ir = "ir(X, X) :- not vrs(X), r(X)."
     exactly_one = ":- vrs(X), irs(X)."
-    return [irs, vrs, vr, ir, exactly_one]
+    return [header, irs, vrs, vr, ir, exactly_one, '']
 
 def euler_region_count(end, start=1):
-    return ["r({}..{}).".format(start, end)]
+    return ["r({}..{}).".format(start, end), '']
 
 rule_count = 0
 
@@ -144,8 +150,8 @@ def gen_tax_rules(root, tax_id=0, concept_count=0):
             for n2 in range(n1+1, len(children)):
                 sd_r.append(gen_sibling_disjointness(children[n1].name, children[n2].name, rule_count))
                 rule_count += 1
-        cov_r = [gen_coverage_rule(root.name, list(map(lambda x: x.name, children)))]
-        concept_r = [gen_concept2_rule(root.name, tax_id)]
+        cov_r.extend([gen_coverage_rule(root.name, list(map(lambda x: x.name, children)))])
+        concept_r.extend([gen_concept2_rule(root.name, tax_id)])
         for child in children:
             isa_r.append(gen_isa_rule(child.name, root.name, rule_count))
             rule_count += 1
@@ -168,7 +174,7 @@ def gen_rules(n1, n2, rels):
     global rule_count
     update_rule_count = False
 
-    rules = []
+    rules = ['% {} {} {}'.format(n1, rels, n2)]
     not_rels = list(all_rels - set(rels))
     rels = list(map(lambda x: sym_dict[x], rels))
     not_rels = list(map(lambda x: sym_dict[x], not_rels))
@@ -196,15 +202,19 @@ def gen_rules(n1, n2, rels):
     if update_rule_count:
         rule_count += 1
 
+    rules.append('')
+
     return rules
 
 def decoding_rules():
 
     rel_list = list(all_rels)
-    mir_rules = [] ##
+    mir_rules = ['% Decoding Rules'] ##
     for i in range(len(rel_list)):
         for j in range(i+1, len(rel_list)):
             mir_rules.append(':- rel(X, Y, "{}"), rel(X, Y, "{}"), concept2(X, N1), concept2(Y, N2).'.format(rel_list[i], rel_list[j]))
+
+    mir_rules.append('')
 
     t = []
     for rel in rel_list:
@@ -212,9 +222,7 @@ def decoding_rules():
     t.append('concept2(X, N1)')
     t.append('concept2(Y, N2)')
     t.append('N1 < N2')
-    t.append('not ncf(X)')
-    t.append('not ncf(Y)')
-    at_least_one_rule = ':- {}.'.format(", ".join(t)) ##
+    at_least_one_rule = ':- {}.\n'.format(", ".join(t)) ##
 
     rel_def = [] ##
     for rel in rel_list:
@@ -223,55 +231,79 @@ def decoding_rules():
             t_.append('{1}hint(X, Y, {0})'.format(i, "" if sym_dict[rel][i] else "not "))
         rel_def.append('rel(X, Y, "{}") :- {}.'.format(rel, ", ".join(t_)))
 
-    ncf_rules = [] ##
+    rel_def.append('')
+
+    hint_def = [] ##
     for i in range(3):
-        ncf_rules.append('hint(X, Y, {}) :- concept2(X, N1), concept2(Y, N2), N1 < N2, vrs(R), {}(X, R), {}(Y, R), not ncf(X), not ncf(Y).'.format(i, idx_rel_map[i][0], idx_rel_map[i][1]))
+        hint_def.append('hint(X, Y, {}) :- concept2(X, N1), concept2(Y, N2), N1 < N2, vrs(R), {}(X, R), {}(Y, R).'.format(i, idx_rel_map[i][0], idx_rel_map[i][1]))
 
     all_rules = mir_rules
     all_rules.append(at_least_one_rule)
     all_rules.extend(rel_def)
-    all_rules.extend(ncf_rules)
+    all_rules.extend(hint_def)
+    all_rules.append('')
     return all_rules
+
+def final_filter(show_rel=True, rcc_conversion=False):
+
+    filter_rules = []
+    if show_rel:
+        filter_rules.append('#show rel/3.')
+    if rcc_conversion:
+        filter_rules.append('eq(A, B) :- rel(A, B, "=").')
+        filter_rules.append('po(A, B) :- rel(A, B, "o").')
+        filter_rules.append('dr(A, B) :- rel(A, B, "!").')
+        filter_rules.append('pi(A, B) :- rel(A, B, "<").')
+        filter_rules.append('pp(A, B) :- rel(A, B, ">").')
+        filter_rules.append('#show eq/2.')
+        filter_rules.append('#show po/2.')
+        filter_rules.append('#show pp/2.')
+        filter_rules.append('#show pi/2.')
+        filter_rules.append('#show dr/2.')
+    return filter_rules
 
 def get_rules(articulations, anytree_):
 
-	rules_to_write = []
+    rules_to_write = []
 
 
-	sibling_disjointness_rules = []
-	coverage_rules = []
-	concept_rules = []
-	isa_rules = []
+    sibling_disjointness_rules = []
+    coverage_rules = []
+    concept_rules = []
+    isa_rules = []
 
-	for i, tax_name in enumerate(anytree_.keys()):
-	    root = anytree_[tax_name][tax_name].children[0]
-	    all_rules = gen_tax_rules(root, tax_id=i)
-	    sibling_disjointness_rules.extend(all_rules[0])
-	    coverage_rules.extend(all_rules[1])
-	    concept_rules.extend(all_rules[2])
-	    isa_rules.extend(all_rules[3])
+    for i, tax_name in enumerate(anytree_.keys()):
+        root = anytree_[tax_name][tax_name].children[0]
+        all_rules = gen_tax_rules(root, tax_id=i)
+        sibling_disjointness_rules.extend(all_rules[0])
+        coverage_rules.extend(all_rules[1])
+        concept_rules.extend(all_rules[2])
+        isa_rules.extend(all_rules[3])
 
-	articulation_rules = []
-	for idx, row in articulations.iterrows():
-	    n1 = row[NODE1_COL]
-	    rel = row[REL_COL].split(",")
-	    n2 = row[NODE2_COL]
-	    if rel[0] != PARENT:
-	        articulation_rules.extend(gen_rules(n1, n2, rel))
+    articulation_rules = ['% Articulations', '']
+    for idx, row in articulations.iterrows():
+        n1 = row[NODE1_COL]
+        rel = row[REL_COL].split(",")
+        n2 = row[NODE2_COL]
+        if rel[0] != PARENT:
+            articulation_rules.extend(gen_rules(n1, n2, rel))
 
-	decoding_rules_ = decoding_rules()
-	init_rules = initial_rules()
+    decoding_rules_ = decoding_rules()
+    init_rules = initial_rules()
 
-	rules_to_write.extend(concept_rules)
-	rules_to_write.extend(init_rules)
-	rules_to_write.extend(sibling_disjointness_rules)
-	rules_to_write.extend(coverage_rules)
-	rules_to_write.extend(isa_rules)
-	rules_to_write.extend(articulation_rules)
-	rules_to_write.extend(decoding_rules_)
-	rules_to_write.append("#show rel/3.")
+    rules_to_write.extend(init_rules)
+    rules_to_write.append('')
+    rules_to_write.append('% Taxonomy Description\n')
+    rules_to_write.extend(concept_rules)
+    rules_to_write.append('')
+    rules_to_write.extend(isa_rules)
+    rules_to_write.extend(coverage_rules)
+    rules_to_write.extend(sibling_disjointness_rules)
+    rules_to_write.extend(articulation_rules)
+    rules_to_write.extend(decoding_rules_)
+    rules_to_write.extend(final_filter())
 
-	return rules_to_write
+    return rules_to_write
 
 
 
