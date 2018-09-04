bf78eb5521efc88136f3c2b59825824e74fcbf8b
sjgupta2 <sjgupta2@illinois.edu>
2018-09-04 09:21:48 -0500

    modularized euler input viz

--- cleanTax_parse.py
+++ cleanTax_parse.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python3
 
-import sys
-import time
-from sys import argv
-from antlr4 import *
+import os
+import pickle
+
 from CleanTaxLexer import CleanTaxLexer
 from CleanTaxParser import CleanTaxParser
 from CleanTaxListener import CleanTaxListener
+from helper import mkdir_p
+
+from antlr4 import *
 import pandas as pd
-import numpy as np
-from antlr4.tree.Trees import Trees
-import os
-import string
 import argparse
-import pickle
-from anytree import Node, RenderTree
-from graphviz import Digraph
-from helper import lineno, isfloat, mkdir_p
-import networkx as nx
+from anytree import Node
 
 PICKLE_FOLDER = "Temp_Pickle_Data"
 CLINGO_INPUT_FOLDER = 'Preprocessed_CleanTax_Input'
@@ -32,19 +26,13 @@ EQUALS = "="
 DISJOINT = "!"
 OVERLAPS = "o"
 
-# ANYTREE_DATA = None
-# GRAPHVIZ_DATA = None
-# RELATION_DATA = None
-
 
 def gen_node_name(node: str, tax_name: str):
-
     return "\"{}_{}\"".format(tax_name, node)
 
 
 # Convert a given relation to one of the aboe edge types
 def rcc_basic_5_to_edge_type(rl):
-
     if rl in ['><', 'o', 'overlaps']:
         return OVERLAPS
     elif rl in ['<', 'is_included_in']:
@@ -62,13 +50,13 @@ def rcc_basic_5_to_edge_type(rl):
 class AntlrCleanTaxListener(CleanTaxListener):
 
     def __init__(self):
-        self.data = {'taxonomies' : {},
-                     'graphviz_tree' : Digraph(comment='Taxonomies'),
-                     'current_taxonomy': None,
-                     'current_articulation': None,
-                     'articulation_list' : [],
-                     'relation_data' : None,
-                    }
+        self.data = {
+            'taxonomies': {},
+            'current_taxonomy': None,
+            'current_articulation': None,
+            'articulation_list': [],
+            'relation_data': None,
+        }
 
     def enterTax_desc(self, ctx):
 
@@ -79,60 +67,39 @@ class AntlrCleanTaxListener(CleanTaxListener):
             i += 1
 
         self.data['current_taxonomy'] = tax_name[0]
-        # print (tax_name)
-
         self.data['taxonomies'][tax_name[0]] = {tax_name[0]: Node(tax_name[0])}
-        self.data['graphviz_tree'].node(tax_name[0])
 
     def enterTax_sub_desc(self, ctx):
-
-        # print ("HERE")
-        # print (self.data['current_taxonomy'])
-        # print (self.data['taxonomies'])
 
         i = 0
         parent = None
         children = []
         while ctx.TEXT(i) is not None:
-
             node_name = ctx.TEXT(i).getText().strip()
-            # print (node_name)
             if i == 0:
                 parent = node_name
             else:
                 children.append(node_name)
-
-            i+=1
-
-        # print ("parent:", parent)
-        # print ("children:", children)
+            i += 1
 
         if (parent != None) and (parent not in self.data['taxonomies'][self.data['current_taxonomy']]):
-
-            self.data['taxonomies'][self.data['current_taxonomy']][parent] = Node(gen_node_name(parent, self.data['current_taxonomy']), parent = self.data['taxonomies'][self.data['current_taxonomy']][self.data['current_taxonomy']])
-            self.data['graphviz_tree'].node("{}.{}".format(self.data['current_taxonomy'],parent))
-            self.data['graphviz_tree'].edge(tail_name = self.data['current_taxonomy'], head_name = "{}.{}".format(self.data['current_taxonomy'],parent), label = PARENT, type = PARENT)
+            self.data['taxonomies'][self.data['current_taxonomy']][parent] = Node(
+                gen_node_name(parent, self.data['current_taxonomy']),
+                parent=self.data['taxonomies'][self.data['current_taxonomy']][self.data['current_taxonomy']])
             # self.data['articulation_list'].append(("{}".format(self.data['current_taxonomy']), PARENT, "{}.{}".format(self.data['current_taxonomy'], parent)))
 
         for child in children:
-
-            self.data['taxonomies'][self.data['current_taxonomy']][child] = Node(gen_node_name(child, self.data['current_taxonomy']), parent = self.data['taxonomies'][self.data['current_taxonomy']][parent])
-            self.data['graphviz_tree'].node("{}.{}".format(self.data['current_taxonomy'],child))
-            self.data['graphviz_tree'].edge(tail_name="{}.{}".format(self.data['current_taxonomy'],parent),
-                                            head_name="{}.{}".format(self.data['current_taxonomy'],child),
-                                            label=PARENT, type=PARENT)
+            self.data['taxonomies'][self.data['current_taxonomy']][child] = Node(
+                gen_node_name(child, self.data['current_taxonomy']),
+                parent=self.data['taxonomies'][self.data['current_taxonomy']][parent])
             self.data['articulation_list'].append((gen_node_name(parent, self.data['current_taxonomy']), PARENT,
                                                    gen_node_name(child, self.data['current_taxonomy'])))
 
     def exitTax_desc(self, ctx):
-
-        # print(RenderTree(self.data['taxonomies'][self.data['current_taxonomy']][self.data['current_taxonomy']]))
         self.data['current_taxonomy'] = None
 
     def enterRcc5_rel(self, ctx):
-
         if ctx.RCC_BASIC_5 is not None:
-
             self.data['current_relation'] = [rcc_basic_5_to_edge_type(ctx.RCC_BASIC_5().getText().strip())]
 
     def enterRcc32_rel(self, ctx):
@@ -141,15 +108,12 @@ class AntlrCleanTaxListener(CleanTaxListener):
         rls = []
         while ctx.RCC_BASIC_5(i) is not None:
             rls.append(ctx.RCC_BASIC_5(i).getText().strip())
-            i+=1
+            i += 1
 
         rls = list(map(rcc_basic_5_to_edge_type, rls))
         self.data['current_relation'] = rls
 
     def exitArticulation(self, ctx):
-
-        node1 = None
-        node2 = None
 
         if ctx.TEXT(0) is None or ctx.TEXT(1) is None:
             return
@@ -167,29 +131,20 @@ class AntlrCleanTaxListener(CleanTaxListener):
 
         self.data['current_relation'].sort()
         rl_type_str = ','.join(map(str, self.data['current_relation']))
-        self.data['graphviz_tree'].edge(tail_name = "{}.{}".format(tax1, node1), head_name = "{}.{}".format(tax2, node2), label = rl_type_str, type = rl_type_str)
         self.data['articulation_list'].append((gen_node_name(node1, tax1), rl_type_str, gen_node_name(node2, tax2)))
         self.data['current_relation'] = None
 
     def exitCt_input(self, ctx):
-        # print (self.data['taxonomies'])
-        # print (self.data['current_taxonomy'])
-        # global RELATION_DATA
-        # global ANYTREE_DATA
-        # self.data['graphviz_tree'].render(view=True)
-        # print (self.data['graphviz_tree'].source)
-        self.data['relation_data'] = pd.DataFrame(self.data['articulation_list'], columns = ['Node1', 'Relation', 'Node2'])
-        # RELATION_DATA = self.data['relation_data']
-        # ANYTREE_DATA = self.data['taxonomies']
-        # print (RELATION_DATA)
-        # self.data = {}
+        self.data['relation_data'] = pd.DataFrame(self.data['articulation_list'],
+                                                  columns=['Node1', 'Relation', 'Node2'])
 
 
 def __main__():
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--fname", type = str, help = "provide the preprocessed cleantax txt file to parse. Need not provide one if it already exists in the cleanTax_input folder as $project_name.txt")
-    parser.add_argument("project_name", type = str, help = "provide session/project name used while preprocessing")
+    parser.add_argument("-f", "--fname", type=str,
+                        help="provide the preprocessed cleantax txt file to parse. Need not provide one if it already "
+                             "exists in the cleanTax_input folder as $project_name.txt")
+    parser.add_argument("project_name", type=str, help="provide session/project name used while preprocessing")
     args = parser.parse_args()
 
     fname = args.fname
@@ -197,7 +152,8 @@ def __main__():
     if fname == None:
         fname = CLINGO_INPUT_FOLDER + '/' + str(project_name) + '.txt'
     if not os.path.exists(fname):
-        print ("No file by the name {}.txt exists in the clingo_output folder. Please recheck the project name.".format(project_name))
+        print("No file by the name {}.txt exists in the clingo_output folder. Please recheck the project name.".format(
+            project_name))
         exit(1)
 
     file = FileStream(fname)
@@ -212,7 +168,6 @@ def __main__():
 
 
 def parse_cleantax(filestream):
-
     lexer = CleanTaxLexer(filestream)
     stream = CommonTokenStream(lexer)
     parser = CleanTaxParser(stream)
@@ -222,7 +177,6 @@ def parse_cleantax(filestream):
     walker = ParseTreeWalker()
     walker.walk(clean_tax_listener, tree)
 
-    # Could be a potential bug where this is deleted as the object goes out of scope
     return clean_tax_listener.data['relation_data'], clean_tax_listener.data['taxonomies']
 
 
--- lean_euler.py
+++ lean_euler.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python3
 from preprocess_clean_tax import preprocess_clean_tax
 from generate_ASP import generate_asp
-from cleanTax_parse import parse_cleantax
+import cleanTax_parse
 import os
 from antlr4 import FileStream
 
 
-def gen_asp_from_cleantax(clean_tax_lines, encoding='mnpw', reasoner='clingo'):
+class LeanEuler:
 
-    preprocessed_clean_tax = preprocess_clean_tax(clean_tax_lines)
-    fname = 'jkbdbcjbdhscbjhbsnkjbjshudhcbskncbhsdbcsbdhbcsbcbshjbdcsjbiueryanballps.txt'
-    #clean_tax_stream = StringIO()
-    #clean_tax_stream.write('\n'.join(preprocessed_clean_tax))
-    with open(fname, 'w') as f:
-        f.write('\n'.join(preprocessed_clean_tax))
-    clean_tax_stream = FileStream(fname)
-    relation_data, anytree_data = parse_cleantax(clean_tax_stream)
-    asp_rules = generate_asp(relation_data, anytree_data, encoding)
-    os.remove(fname)
+    @staticmethod
+    def parse_cleantax(clean_tax_lines):
 
-    return asp_rules, relation_data, anytree_data
+        preprocessed_clean_tax = preprocess_clean_tax(clean_tax_lines)
+        fname = 'jkbdbcjbdhscbjhbsnkjbjshudhcbskncbhsdbcsbdhbcsbcbshjbdcsjbiueryanballps.txt'
+        # clean_tax_stream = StringIO()
+        # clean_tax_stream.write('\n'.join(preprocessed_clean_tax))
+        with open(fname, 'w') as f:
+            f.write('\n'.join(preprocessed_clean_tax))
+        clean_tax_stream = FileStream(fname)
+        os.remove(fname)
+        return cleanTax_parse.parse_cleantax(clean_tax_stream)
+
+    @staticmethod
+    def gen_asp_rules(relation_data, anytree_data, encoding='mnpw', reasoner='clingo'):
+        return generate_asp(relation_data, anytree_data, encoding)
+
+    @staticmethod
+    def gen_asp_rules_from_cleantax(clean_tax_lines, encoding='mnpw', reasoner='clingo'):
+        relation_data, anytree_data = LeanEuler.parse_cleantax(clean_tax_lines)
+        return LeanEuler.gen_asp_rules(relation_data, anytree_data, encoding, reasoner)
